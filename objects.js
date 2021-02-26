21d64999fa34de9ebb0f2dc24e43fc86645c208a
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-25 20:54:39 -0800

    Worked on objects and my hw

---
+++ objects.js
@@ -0,0 +1,15 @@
+Object;
+let person = {
+    name: "Sally",
+    lastName: "Smith", 
+    age: 24,
+school: "Duke University", 
+state: "North Carolina",
+favFood: "grapes",
+};
+
+console.log('name:', person.name);
+console.log('last name:' , person.lastName);
+console.log('age:' , person.age);
+console.log('school:' , person.school);
+console.log('favorite food:' , person.favFood);
