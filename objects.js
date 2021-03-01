d351982d578c9ab90f8d6cd857833c3fd35fc0a5
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-28 18:39:33 -0800

    added more to my objects

--- objects.js
+++ objects.js
@@ -1,15 +1,24 @@
 Object;
 let person = {
-    name: "Sally",
-    lastName: "Smith", 
+    name: 'Sally',
+    lastName: 'Smith', 
     age: 24,
-school: "Duke University", 
-state: "North Carolina",
-favFood: "grapes",
+school: 'Duke University', 
+state: 'North Carolina',
+favFood: 'grapes',
+favSubjects: ['history', 'math', 'science'],
+family: ['anna', 'brick']
 };
+
 
 console.log('name:', person.name);
 console.log('last name:' , person.lastName);
 console.log('age:' , person.age);
 console.log('school:' , person.school);
 console.log('favorite food:' , person.favFood);
+console.log('favorite subjects:' , person.favSubjects[0]);
+console.log('favorite subjects:' , person.favSubjects[1]);
+console.log('favorite subjects:' , person.favSubjects[2]);
+console.log('family:' , person.family[0]);
+console.log('family:' , person.family[1]);
+
